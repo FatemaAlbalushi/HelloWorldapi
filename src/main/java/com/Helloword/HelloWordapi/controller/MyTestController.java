258df3c819144fcc1fee0aa58e9bf2e81a630f92
FatemaAlbalushi <fatoomsaid188@gmail.com>
2023-02-26 14:23:19 +0400

    Hello World API

--- src/main/java/com/Helloword/HelloWordapi/controller/MyTestController.java
+++ src/main/java/com/Helloword/HelloWordapi/controller/MyTestController.java
@@ -1,19 +1,32 @@
 package com.Helloword.HelloWordapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
+        import java.util.HashMap;
 
 @RestController
-@RequestMapping(path = "/1")
+
 public class MyTestController {
     @GetMapping(path = "/api")
-    public HashMap<String,String> giveQuote(){
+    public HashMap<String,String> giveQuote(@RequestParam(value = "name", defaultValue = "Mr") String name){
         HashMap<String,String> response= new HashMap<String,String>();
-        response.put("status", "Ok");
-        response.put("result","Hello World" );
+        try {
+            // simulate a blocking operation
+            response.put("status", "200");
+            response.put("result","Hello " + name );
+        } catch (Exception e) {
+            // handle any exceptions that may occur during the blocking operation
+            response.put("status", "Error");
+            response.put("message", "An error occurred while processing your request.");
+        }
         return response;
     }
+
+    @GetMapping(path = "/api/hw")
+    public String getHome(){
+        return "Hello World!";
+    }
 }
+
